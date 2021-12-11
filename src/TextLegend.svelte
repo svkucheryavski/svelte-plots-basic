06b0c4258cafe739ac0aa735b51bda8da0dce9a4
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2021-12-11 13:29:11 +0100

    improvements to coorditane handling in TextLegend component

--- src/TextLegend.svelte
+++ src/TextLegend.svelte
@@ -1,27 +1,38 @@
 <script>
-   import TextLabels from './TextLabels.svelte';
+   import { getContext } from 'svelte';
    import { Colors } from './Colors';
 
    /* input parameters */
-	export let x;
-   export let y;
+	export let left;
+   export let top;
    export let dx = "0";
    export let dy = "1.25em";
    export let elements;
-   export let pos = 0;
    export let faceColor = Colors.PRIMARY_TEXT;
    export let borderColor = "transparent";
    export let borderWidth = 0;
    export let textSize = 1;
 
-   let labelsStr = "";
+   // get axes context and reactive variables needed to compute coordinates
+   const axes = getContext('axes');
+   const xLim = axes.xLim;
+   const yLim = axes.yLim;
+   const axesWidth = axes.width;
+   const axesHeight = axes.height;
+   const scale = axes.scale;
 
-   $: {
-      labelsStr = "";
-      for (let i = 0; i < elements.length; i++) {
-         labelsStr += `<tspan x=0 dx=${dx} dy=${i === 0 ? 0 : dy}>${elements[i]}</tspan>`
-      }
-   }
+   // reactive variables for coordinates of data points in pixels
+   $: x = axes.scaleX([left], $xLim, $axesWidth);
+   $: y = axes.scaleY([top], $yLim, $axesHeight);
+
+   // styles for bars and labels
+   $: textStyleStr = `fill:${faceColor};stroke-width:${borderWidth}px;stroke:${borderColor};font-size:${textSize}em;`;
 </script>
 
-<TextLabels {textSize} {borderColor} {borderWidth} {faceColor} xValues={[x]} yValues={[y]} {pos} labels={labelsStr} />
+{#if x !== undefined && y !== undefined && elements.length > 0}
+   <text style={textStyleStr} x={x} y={y} dx={dx} dy={dy} dominant-baseline="middle" text-anchor="start">
+      {#each elements as el, i}
+         <tspan x={x} dx={dx} dy={i === 0 ? 0 : dy}>{@html el}</tspan>
+      {/each}
+   </text>
+{/if}
